e48ee67f797b681e029f5d8b2076e7557ad1dc56
Jahmilli <sebastian.southern@gmail.com>
2020-04-10 15:50:20 +1000

    additional fixes, build and deploy working, contains test GET endpoint

--- src/main/java/ses/teamsbackend/Application.java
+++ src/main/java/ses/teamsbackend/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TeamsBackendApplication {
+public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TeamsBackendApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 
 }
---
+++ src/main/java/ses/teamsbackend/controllers/TestController.java
@@ -0,0 +1,15 @@
+package ses.teamsbackend.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+  @GetMapping("/")
+  public String getString() {
+    return "This is a string";
+  }
+}
