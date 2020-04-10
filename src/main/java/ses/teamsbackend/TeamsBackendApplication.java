6f310de31884f2d524ddce65426af332219e412b
Jahmilli <sebastian.southern@gmail.com>
2020-04-10 14:46:39 +1000

    initial application setup

---
+++ src/main/java/ses/teamsbackend/TeamsBackendApplication.java
@@ -0,0 +1,13 @@
+package ses.teamsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ses/teamsbackend/TeamsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ses.teamsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
