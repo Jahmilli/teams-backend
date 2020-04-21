638e1035361bcbbabb4306f8d43d7a5ad9fab9b2
Akshat Singh <akshatsingh99@gmail.com>
2020-04-21 20:20:45 +1000

    Branch Init with bootstrap Chat code

---
+++ src/main/java/ses/teamsbackend/controllers/MessageController.java
@@ -0,0 +1,25 @@
+package ses.teamsbackend.controllers;
+
+import ses.teamsbackend.model.MessageModel;
+import ses.teamsbackend.storage.UserStorage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @MessageMapping("/chat/{to}")
+    public void sendMessage(@DestinationVariable String to, MessageModel message) {
+        System.out.println("handling send message: " + message + " to: " + to);
+        boolean isExists = UserStorage.getInstance().getUsers().contains(to);
+        if (isExists) {
+            simpMessagingTemplate.convertAndSend("/topic/messages/" + to, message);
+        }
+    }
+}
--- src/main/java/ses/teamsbackend/controllers/TestController.java
+++ src/main/java/ses/teamsbackend/controllers/TestController.java
@@ -1,7 +1,7 @@
 package ses.teamsbackend.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
